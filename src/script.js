834d4e28ea45f441a024e7a636446808d6b2ae29
Tri <trimpham98@gmail.com>
2023-07-16 23:04:36 -0700

    Cast a ray in tick function

--- src/script.js
+++ src/script.js
@@ -106,6 +106,16 @@ const tick = () => {
   object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
   object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5;
 
+  // Cast a ray
+  const rayOrigin = new THREE.Vector3(-3, 0, 0);
+  const rayDirection = new THREE.Vector3(1, 0, 0);
+  rayDirection.normalize();
+
+  raycaster.set(rayOrigin, rayDirection);
+
+  const objectsToTest = [object1, object2, object3];
+  const intersects = raycaster.intersectObjects(objectsToTest);
+
   // Update controls
   controls.update();
 
