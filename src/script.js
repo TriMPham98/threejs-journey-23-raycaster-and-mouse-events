819bbcfa2e12cb22d605536b1d4f04fa48609524
Tri <trimpham98@gmail.com>
2023-07-16 22:33:49 -0700

    Instantiate ray origin and direction and normalize into unit vector

--- src/script.js
+++ src/script.js
@@ -43,6 +43,12 @@ scene.add(object1, object2, object3);
  */
 const raycaster = new THREE.Raycaster();
 
+const rayOrigin = new THREE.Vector3(-3, 0, 0);
+const rayDirection = new THREE.Vector3(10, 0, 0);
+rayDirection.normalize();
+
+raycaster.set(rayOrigin, rayDirection);
+
 /**
  * Sizes
  */
