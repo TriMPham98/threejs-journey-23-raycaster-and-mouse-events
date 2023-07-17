a8728016ac70c0a17b127f5bf8259cf084efa3ae
Tri <trimpham98@gmail.com>
2023-07-16 23:00:36 -0700

    Animate object2 and 3

--- src/script.js
+++ src/script.js
@@ -103,6 +103,8 @@ const tick = () => {
 
   // Animate objects
   object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5;
+  object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
+  object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5;
 
   // Update controls
   controls.update();
