2e72e2c9e54240f13d6b7e66f08a034e296b1145
Tri <trimpham98@gmail.com>
2023-07-20 23:15:54 -0700

    Import GLTFLoader

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader.js";
 import * as dat from "lil-gui";
 
 THREE.ColorManagement.enabled = false;
