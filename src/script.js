925f65ed2bd9982435ed51ddc1b505bcfe45415d
Tri <trimpham98@gmail.com>
2023-07-20 21:06:09 -0700

    Instantiate witness variable

--- src/script.js
+++ src/script.js
@@ -109,6 +109,8 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
  */
 const clock = new THREE.Clock();
 
+let currentIntersect = null;
+
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
